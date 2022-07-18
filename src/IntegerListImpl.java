cbba48d30baf15c0de2cf250552c7e7d493c3fcf
Lexxkit <lexxkit@gmail.com>
2022-07-18 13:42:40 +0100

    implemented quickSort instead of sortInsertion;

--- src/IntegerListImpl.java
+++ src/IntegerListImpl.java
@@ -90,7 +90,7 @@ public class IntegerListImpl implements IntegerList {
             return false;
         }
         Integer[] storageCopy = toArray();
-        sortInsertion(storageCopy);
+        quickSort(storageCopy, 0, size - 1);
 
 
         return binarySearch(storageCopy, item);
@@ -196,4 +196,36 @@ public class IntegerListImpl implements IntegerList {
         }
         return false;
     }
+
+    private void quickSort(Integer[] arr, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
+        }
+    }
+
+    private int partition(Integer[] arr, int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                swapElements(arr, i, j);
+            }
+        }
+        swapElements(arr, i + 1, end);
+        return i + 1;
+    }
+
+    private void swapElements(Integer[] arr, int left, int right) {
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+    }
+
+
 }
